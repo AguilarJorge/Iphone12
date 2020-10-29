b9f143dcfd11a1477b2e58415fb20d8f569bb34b
Jorge Aguilar <jorgeluis_942009@hotmail.com>
2020-10-29 16:32:35 -0600

    ui

--- script.js
+++ script.js
@@ -4,205 +4,212 @@ $(function(){
     apps: [
       {
         nombre: 'Calendario',
-        icono: './appsIcon/calendario.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fcalendario.png?alt=media&token=ae918b56-05c3-40a1-be64-1abcdb291c6f',
         type: 'widgetFull',
         dinamico: true
       },
       {
         nombre: 'Clima',
-        icono: './appsIcon/clima.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fclima.png?alt=media&token=02edb357-5775-4bf1-91c0-8e3f4908ddea',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'FaceTime',
-        icono: './appsIcon/facetime.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Ffacetime.png?alt=media&token=4c66d073-d44e-4671-878e-6a8dd1dc7956',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Calendario',
-        icono: './appsIcon/calendario.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fcalendario.png?alt=media&token=ae918b56-05c3-40a1-be64-1abcdb291c6f',
         type: 'app',
         dinamico: true
       },
       {
         nombre: 'Reloj',
-        icono: './appsIcon/reloj.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Freloj.png?alt=media&token=d0d76558-79c6-4d21-8fe7-1d13de3a6a3e',
         type: 'app',
         dinamico: true
       },
       {
         nombre: 'Fotos',
-        icono: './appsIcon/fotos.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Ffotos.png?alt=media&token=86d712fd-aab1-48a3-a6d0-f2b5b7f9a2ab',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Mapas',
-        icono: './appsIcon/maps.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fmaps.png?alt=media&token=309018b2-981a-44fd-bf97-8da765fef711',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Cámara',
-        icono: './appsIcon/camara.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fcamara.png?alt=media&token=bd81cee7-b011-4773-a053-a7cd935e34c3',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Recordatorios',
-        icono: './appsIcon/recordatorios.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Frecordatorios.png?alt=media&token=2dbccbc3-374b-4eed-aa00-68db99217b7d',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Facebook',
-        icono: './appsIcon/facebook.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Ffacebook.png?alt=media&token=f383d17e-32a7-49ef-8ebb-c723d556baa2',
         type: 'app',
+        notificaciones: 5,
         dinamico: false
       },
       {
         nombre: 'Notas',
-        icono: './appsIcon/notas.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fnotas.png?alt=media&token=7a25ae1c-9010-48e4-81e8-e04a904024e9',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'App Store',
-        icono: './appsIcon/appstore.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fappstore.png?alt=media&token=fe121a13-738f-43cd-936b-45ee7fb90cec',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Salud',
-        icono: './appsIcon/salud.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fsalud.png?alt=media&token=fee7445d-600c-4146-a590-1adc1d40001b',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Mensajes',
-        icono: './appsIcon/mensajes.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fmensajes.png?alt=media&token=e389ae38-bbeb-4ca9-aa46-4be8a5b6ca90',
+        notificaciones: 123,
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Configuración',
-        icono: './appsIcon/configuracion.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fconfiguracion.png?alt=media&token=dfe6b4dd-0b50-4a78-8ef1-22728ed9840c',
         type: 'app',
+        notificaciones: 3,
         dinamico: false
       },
       {
         nombre: 'BBVA',
-        icono: './appsIcon/bbva.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fbbva.png?alt=media&token=840b9fa7-c765-4147-9138-8234eabfeb9e',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'WhatsApp',
-        icono: './appsIcon/whatsapp.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fwhatsapp.png?alt=media&token=b3416a44-56fc-4991-a10b-e4a34944bf3c',
         type: 'app',
+        notificaciones: 22,
         dinamico: false
       },
       {
         nombre: 'Calculadora',
-        icono: './appsIcon/calculadora.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fcalculadora.png?alt=media&token=0ce89e65-2e11-408c-8467-39637f0f19f8',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Twitter',
-        icono: './appsIcon/twitter.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Ftwitter.png?alt=media&token=4360a8ea-afce-4f20-9057-bb2d4d5a9e41',
         type: 'app',
+        notificaciones: 2,
         dinamico: false
       },
       {
         nombre: 'Messenger',
-        icono: './appsIcon/messenger.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fmessenger.png?alt=media&token=5de5ef16-9bbf-4d79-b9d2-2e416106b21c',
+        notificaciones: 3,
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Brújula',
-        icono: './appsIcon/brujula.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fbrujula.png?alt=media&token=b4e4b1bb-1048-427d-836e-7d8b599d71e0',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Spotify',
-        icono: './appsIcon/spotify.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fspotify.png?alt=media&token=f538e504-a8d7-4707-a308-8d4bc2b6f7f6',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Pinterest',
-        icono: './appsIcon/pinterest.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fpinterest.png?alt=media&token=4ad25035-0875-4000-ba39-5541234494c1',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Google Home',
-        icono: './appsIcon/home.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fhome.png?alt=media&token=b6f455b0-7333-4c4a-b866-56e900beb41e',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Liverpool',
-        icono: './appsIcon/liverpool.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fliverpool.png?alt=media&token=42dfbbba-2149-4fe0-b503-419513082039',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Musica',
-        icono: './appsIcon/musica.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fmusica.png?alt=media&token=4ee18c65-11d5-46bb-8c4f-3536e49e33b0',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Netflix',
-        icono: './appsIcon/netflix.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fnetflix.png?alt=media&token=88ddda95-de2f-44e6-b8a1-d5016f801e1c',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Notas de voz',
-        icono: './appsIcon/notasvoz.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fnotasvoz.png?alt=media&token=dddc5de3-e91f-41da-8670-26ccddf54471',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Wallet',
-        icono: './appsIcon/wallet.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fwallet.png?alt=media&token=96369c3a-d6f4-434f-9049-7b934c35af60',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Podcasts',
-        icono: './appsIcon/podcast.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fpodcast.png?alt=media&token=166e653d-d060-46e6-9274-3a4d368d7ec4',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Archivos',
-        icono: './appsIcon/archivos.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Farchivos.png?alt=media&token=b4ce6b1b-9877-41ca-a0f3-b05526591495',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Contactos',
-        icono: './appsIcon/contactos.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fcontactos.png?alt=media&token=fe1b3525-ef43-4a68-91ec-82b3f7b6b979',
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'YouTube',
-        icono: './appsIcon/youtube.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Fyoutube.png?alt=media&token=180f2d4c-32bf-4d40-855c-c70148d727e9',
+        notificaciones: 1,
         type: 'app',
         dinamico: false
       },
       {
         nombre: 'Buscar iPhone',
-        icono: './appsIcon/findphone.png',
+        icono: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/ios14%2Ffindphone.png?alt=media&token=9b0106d2-b151-49af-a3d9-dfbd64bc4421',
         type: 'app',
         dinamico: false
       }
@@ -223,19 +230,21 @@ $(function(){
     touchMov: function(config){
       config = jQuery.extend({
         mov: 'x',
-        threshold: 50,
-        movIzq: function () {console.log('movio a la izquierda')},
-        movDer: function () {console.log('movio a la derecha')},
-        movUp: function () { console.log('movio hacia arriba') },
-        movDown: function () { console.log('movio hacia abajo') },
-        updateMovX: function (e, mov) { console.log('se esta moviendo en x', mov) },
-        updateMovY: function (e, mov) { console.log('se esta moviendo en y', mov) },
-        finishMov: function () { console.log('Termino de mover') }
+        movIzq: function(){},
+        movDer: function(){},
+        movUp: function(){},
+        movDown: function(){},
+        updateMovX: function(){},
+        updateMovY: function(){},
+        finishMov: function(){}
       }, config);
       let el = this;
-      let initCoords, movCoords = { x: 0, y: 0 };
+      let initCoords = { x: 0, y: 0 };
+      let movCoords = { x: 0, y: 0 };
+      let downCoords = { x: 0, y: 0 };
       el.mousedown(function (e) {
         initCoords = { x: e.pageX, y: e.pageY };
+        downCoords = { x: movCoords.x, y: movCoords.y };
         el.mousemove(function (e2) {
           movCoords = { x: e2.pageX, y: e2.pageY };
           if (config.mov === 'x') {
@@ -246,9 +255,13 @@ $(function(){
         })
         el.mouseup(function (ex) {
           if (config.mov === 'x') {
-            (movCoords.x - initCoords.x) > 0 ? config.movDer(ex) : config.movIzq(ex);
+            if (movCoords.x - downCoords.x != 0) {
+              (movCoords.x - initCoords.x) > 0 ? config.movDer(ex) : config.movIzq(ex);
+            }
           } else if (config.mov === 'y') {
-            (movCoords.y - initCoords.y) > 0 ? config.movDown(ex) : config.movUp(ex);
+            if (movCoords.y - downCoords.y != 0) {
+              (movCoords.y - initCoords.y) > 0 ? config.movDown(ex) : config.movUp(ex);
+            }
           }
           config.finishMov(ex);
           el.off('mousemove');
@@ -257,9 +270,13 @@ $(function(){
         })
         el.mouseleave(function (a) {
           if (config.mov === 'x') {
-            (movCoords.x - initCoords.x) > 0 ? config.movDer(a) : config.movIzq(a);
+            if (movCoords.x - downCoords.x != 0) {
+              (movCoords.x - initCoords.x) > 0 ? config.movDer(a) : config.movIzq(a);
+            }
           } else if (config.mov === 'y') {
-            (movCoords.y - initCoords.y) > 0 ? config.movDown(a) : config.movUp(a);
+            if (movCoords.y - downCoords.y != 0) {
+              (movCoords.y - initCoords.y) > 0 ? config.movDown(a) : config.movUp(a);
+            }
           }
           config.finishMov(a);
           el.off('mousemove');
@@ -378,6 +395,7 @@ $(function(){
       if (app.type == 'widgetFull') clases = clases + ' widgetFull';
       if (app.dinamico && app.type == 'app') clases = `${clases} ${app.nombre.toLowerCase()}Dinamico`;
       html += `<div class="${clases}" data-app="${app.type+app.nombre}">
+                ${app.notificaciones ? `<div class="notificacion">${app.notificaciones}</div>` : ''}
                 <div class="icono" style="${!app.dinamico ? `background-image:url(${app.icono});` : 'background-color:#fff;'}"></div>
                 <p class="nombre">${app.nombre}</p>
       </div>`;
@@ -396,14 +414,24 @@ $(function(){
     container.append(html);
   }
 
+  //Pintamos todas las apps en el contenedor principal
   pintarApps(globalState.apps, $('.wrapperApps'), $('.wrapperDots'));
+  //Preparamos el widget del calendario
   $('.wrapperApps .app[data-app="widgetFullCalendario"] .icono').append('<div class="eventos"><p>Sin más eventos para hoy</p></div><div class="calendarioWrapper"></div>');
+  //Creamos el calendario del widget
   $('.wrapperApps .app[data-app="widgetFullCalendario"] .icono .calendarioWrapper').calendario();
+  //Icono dinamico del calendario
   $('.wrapperApps .app.calendarioDinamico .icono').fechaIcono();
+  //Reloj analogico dinamico
   $('.wrapperApps .app.relojDinamico .icono').reloj();
+  //Hora de la statusBar
   $('.statusBar .hora').hora();
+  //Hora de la pantalla de bloqueo
   $('.lockScreen .hora').hora();
+  //Fecha de la pantalla de bloqueo
   $('.lockScreen .fecha').fecha();
+  //Eventos del dia en la pantalla de widgetsCenter
+  $('.widgetCenter .block.eventos').fechaIcono({diaCompleto: true});
 
   //Touch actions
   $('.lockScreen').touchMov({
@@ -445,7 +473,7 @@ $(function(){
           transition: 'ease all 0.2s'
         });
       } else {
-        console.log($(e.currentTarget));
+        $(e.currentTarget).parents('.mainScreen').addClass('blur');
         $(e.currentTarget).parents('.appScreen').addClass('moveOut');
         $(e.currentTarget).parents('.appScreen').siblings('.widgetCenter').removeClass('hidden');
         $(e.currentTarget).css({
@@ -468,4 +496,87 @@ $(function(){
       globalState.wrapperApps.transform = transform;
     }
   });
+  $('.widgetCenter .contenido').touchMov({
+    mov: 'x',
+    movIzq: function (e) {
+      $(e.currentTarget).parents('.mainScreen').removeClass('blur');
+      $(e.currentTarget).parent().addClass('hidden');
+      $(e.currentTarget).parent().siblings('.appScreen.moveOut').removeClass('moveOut');
+    },
+    updateMovX: function (e, mov) {
+      if (Math.sign(mov) == 1) {
+        $(e.currentTarget).parent().css({
+          transform: `translateX(${mov}px)`,
+          transition: 'none'
+        });
+      }
+    },
+    movDer: function(e){
+      $(e.currentTarget).parent().css({
+        transform: 'none',
+        transition: 'ease all .2s'
+      });
+      setTimeout(() => {
+        $(e.currentTarget).parent().removeAttr('style');
+      }, 200)
+    }
+  });
+  //Mostrar icono para eliminar apps (shaking apps)
+  $('.mainScreen .appScreen').mousedown(function(e){
+    e.stopPropagation();
+    if ($(this).parent().hasClass('shakingApps')) return false;
+    let timeout = setTimeout(() => {
+      if ($(e.target).hasClass('app') || $(e.target).parents('.app').length) {
+        $(this).parent().addClass('filterBlur');
+        let app;
+        if ($(e.target).hasClass('app')) {
+          app = $(e.target);
+        } else {
+          app = $(e.target).parents('.app');
+        }
+        let appClon = app.clone();
+        appClon.attr('id', 'fixedApp');
+        console.log(app);
+        console.log(app[0].getBoundingClientRect());
+        appClon.css({
+          top: app[0].getBoundingClientRect().top,
+          left: app[0].getBoundingClientRect().left,
+          width: app[0].getBoundingClientRect().width
+        })
+        $('body').append(appClon);
+        $('body').append(`
+          <div class="fixedMenuFixedApp">
+            <div class="menuOption eliminar">Eliminar app
+              <div class="icono">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+                  <circle cx="32" cy="32" r="30"></circle>
+                  <path d="M48 32H16"></path>
+                </svg>
+              </div>
+            </div>
+            <div class="menuOption">Editar pantalla de inicio
+              <div class="icono">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+                  <path d="M14 59a3 3 0 0 0 3 3h30a3 3 0 0 0 3-3v-9H14zM50 5a3 3 0 0 0-3-3H17a3 3 0 0 0-3 3v5h36zm0 45V10m-36 0v40"></path>
+                  <circle cx="32" cy="56" r="2"></circle>
+                </svg>
+              </div>
+            </div>
+          </div>
+        `);
+      } else {
+        $(this).parent().addClass('shakingApps');
+        $('.appScreen .app').append('<div class="removeApp"></div>');
+      }
+    }, 1000);
+    $(this).mouseup(function(){
+      clearTimeout(timeout);
+    })
+  })
+  //Salir del eliminador de apps (shaking apps)
+  $('.exitShake').click(function(){
+    $('.mainScreen').removeClass('shakingApps');
+    $('.appScreen .app .removeApp').remove();
+  })
+
 })
